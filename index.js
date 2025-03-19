029d0265fcdef84c81357e43224d13a2b8d2a776
Brian <brian.sila1@student.moringaschool.com>
2025-03-19 13:52:59 +0300

    phase-1-sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+    const data = {
+        name: name,
+        email: email,
+    };
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(object => {
+        const body = document.querySelector("body");
+        const idElement = document.createElement("p");
+        idElement.textContent = object.id;
+        body.appendChild(idElement);
+    })
+    .catch(error => {
+        const body = document.querySelector("body");
+        const errorElement = document.createElement("p");
+        errorElement.textContent = error.message;
+        body.appendChild(errorElement);
+    });
+}
+
